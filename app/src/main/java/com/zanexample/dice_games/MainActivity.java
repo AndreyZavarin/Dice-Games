71a4ba7640f391c14837781053ed0cfd3c984dc1
Евгений <montpeu@mail.ru>
2017-02-10 09:49:30 +0400

    analyze of combinations

--- app/src/main/java/com/zanexample/dice_games/MainActivity.java
+++ app/src/main/java/com/zanexample/dice_games/MainActivity.java
@@ -12,7 +12,10 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.Arrays;
 import java.util.Random;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,15 +23,23 @@ public class MainActivity extends AppCompatActivity {
     //TextView tv_cpu, tv_player;
     ImageView iv_ir_1, iv_ir_2, iv_ir_3, iv_ir_4, iv_ir_5;
     Button doThrow;
+    Button writeScore;
     //int cpuPoints = 0, playerPoints = 0;
     Random rndm;
-
+    TextView tvPlayerCount;
+    TextView tvCurrentScore;
     int cube_1, cube_2, cube_3, cube_4, cube_5;
     int scoreOfThrow=0;
     int tmpForSearchCombs = 0;
-    int[] valueOnCubes = {cube_1,cube_2,cube_3,cube_4,cube_5};
-    //?????????????????????????????
+    int defaultNumberOfCubes = 5;
+    int numberOfCubes = defaultNumberOfCubes;
+    int[] valueOnCubes = new int[numberOfCubes];
+    //Разобраться, почему выбивает эксепшн, при другом объявлении
     int[] counter = {0,0,0,0,0,0,0};
+    int scoreOfCurrentThrow;
+    int[] resultOfAnazlyzeCubs;
+    int playerScore=0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,52 +51,142 @@ public class MainActivity extends AppCompatActivity {
         iv_ir_5 = (ImageView) findViewById(R.id.ir_05);
 
         doThrow = (Button) findViewById(R.id.btnDoThrow);
-        rndm = new Random();
+        writeScore = (Button) findViewById(R.id.btnWriteScore);
+
+        tvPlayerCount = (TextView) findViewById(R.id.player_count);
+        tvCurrentScore = (TextView) findViewById(R.id.currentScore);
 
         final ImageView[] arrayOfIVCubes = {iv_ir_1, iv_ir_2, iv_ir_3, iv_ir_4, iv_ir_5};
-/*
-        for(int i=0; i < valueOnCubes.length; i++){
-            valueOnCubes[i] = rndm.nextInt(6) + 1;
-        }
-*/
+
         doThrow.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-      //          int throwCubes = rndm.nextInt(6) + 1;
-                //setup values of cubes by random
-                for(int i=0; i < valueOnCubes.length; i++){
-                    valueOnCubes[i] = rndm.nextInt(6) + 1;
-                }
 
-                //search of combs (to separate method)
+                valueOnCubes = throwCubes(numberOfCubes);
+
                 //В массиве counter хранится количество повторений значений кубиков
                 for(int i=0; i<valueOnCubes.length; i++){
                     counter[valueOnCubes[i]]++;
                 }
-                System.out.println("value\tcount");
-                for(int i=0; i<counter.length; i++){
-                    System.out.println(i + "\t" + counter[i]);
+                resultOfAnazlyzeCubs = combinations(valueOnCubes, counter);
+
+                numberOfCubes = resultOfAnazlyzeCubs[1];
+                scoreOfCurrentThrow = resultOfAnazlyzeCubs[0];
+
+                //Если кубики кончились, а ход не закончен, бросать заново все кубики
+                if(numberOfCubes == 0){
+                    numberOfCubes = defaultNumberOfCubes;
                 }
+                //Обнуление счёта, если ничего не выпало
+                if(scoreOfCurrentThrow == 0){
+                    scoreOfCurrentThrow = 0;
+                    numberOfCubes = defaultNumberOfCubes;
+                }
+                scoreOfCurrentThrow = scoreOfCurrentThrow + resultOfAnazlyzeCubs[0];
+                tvCurrentScore.setText(String.valueOf(scoreOfCurrentThrow));
+
+
+                System.out.println("Score: " + resultOfAnazlyzeCubs[0]);
+                System.out.println("Count: " + resultOfAnazlyzeCubs[1]);
+
               //zeroing array
                 for(int i=0; i<counter.length; i++){
                    counter[i]=0;
                 }
 
                 Animation rotate = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.rotate);
-                for(int i=0; i< arrayOfIVCubes.length; i++){
+                for(int i=0; i< numberOfCubes; i++){
                     arrayOfIVCubes[i].startAnimation(rotate);
                 }
 
                 setImage(valueOnCubes, arrayOfIVCubes);
-
             }
-
         });
 
-
-
+        writeScore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                playerScore = playerScore + scoreOfCurrentThrow;
+                tvPlayerCount.setText(String.valueOf(playerScore));
+            }
+        });
+    }
+    public int[] throwCubes(int numberOfCubes){
+        rndm = new Random();
+        int[] valueOnThrowCubes = new int[numberOfCubes];
+        //setup values of cubes by random
+        for(int i=0; i < numberOfCubes; i++){
+            valueOnThrowCubes[i] = rndm.nextInt(6) + 1;
+        }
+        return valueOnThrowCubes;
     }
 
+    public int[] combinations(int[] valueOnCubes, int[] counter){
+        String stringCounter;
+        String combination12345 = new String("12345");
+        String combintaion23456 = new String("23456");
+        int scoreOfThrow = 0;
+        int tmpForCountUsedCubs = 0;
+        int[] arrayReturns = {scoreOfThrow, tmpForCountUsedCubs};
+        //Обработка цифры 1 и ее возможных комбинаций.
+        switch (counter[1]){
+            case 1:
+                scoreOfThrow = 10;
+                break;
+            case 2:
+                scoreOfThrow = 20;
+                break;
+            case 3:
+                scoreOfThrow = 100;
+                break;
+            case 4:
+                scoreOfThrow = 200;
+                break;
+            case 5:
+                scoreOfThrow = 1000;
+                break;
+        }
+        //Обработка цифры 5 при одном или двух выпаданиях.
+        switch (counter[5]){
+            case 1:
+                scoreOfThrow = scoreOfThrow + 5;
+                break;
+            case 2:
+                scoreOfThrow = scoreOfThrow + 10;
+                break;
+        }
+
+        for(int i=2; i<counter.length; i++){
+            if(counter[i] == 3){
+                scoreOfThrow = scoreOfThrow + i * 10;
+                tmpForCountUsedCubs = 3;
+            }
+            if(counter[i] == 4){
+                scoreOfThrow = scoreOfThrow +  i * 10 * 2;
+                tmpForCountUsedCubs = 4;
+            }
+            if(counter[i] == 5){
+                scoreOfThrow = scoreOfThrow +  i * 100;
+                tmpForCountUsedCubs = 5;
+            }
+        }
+        Arrays.sort(valueOnCubes);
+        stringCounter = Arrays.toString(valueOnCubes);
+        System.out.println("SortString " + stringCounter);
+        if(stringCounter.equals(combination12345)){
+            scoreOfThrow = 125;
+            tmpForCountUsedCubs = 5;
+        }
+        if(stringCounter.equals(combintaion23456)){
+            scoreOfThrow = 250;
+            tmpForCountUsedCubs = 5;
+        }
+        tmpForCountUsedCubs = tmpForCountUsedCubs + counter[1] + counter [5];
+        tmpForCountUsedCubs = numberOfCubes - tmpForCountUsedCubs;
+        arrayReturns[0]=scoreOfThrow;
+        arrayReturns[1]=tmpForCountUsedCubs;
+        return arrayReturns;
+    }
 
     public void setImage(int[] value, ImageView[] imageView){
         for(int i=0; i < value.length; i++){
@@ -111,9 +212,34 @@ public class MainActivity extends AppCompatActivity {
                 }
         }
     }
-
-
 }
+
+
+
+//                String string = new String("12345");
+//                validation(string);
+//scoreOfCurrentThrow = combinations(valueOnCubes, counter);
+                /*System.out.println("PLAYER_COUNT" + tmp);
+                System.out.println("value\tcount");
+                for(int i=0; i<counter.length; i++){
+                    System.out.println(i + "\t" + counter[i]);
+                }*/
+/*
+        for(int i=0; i < valueOnCubes.length; i++){
+            valueOnCubes[i] = rndm.nextInt(6) + 1;
+        }
+*/
+
+/*/    public void validation(String string){
+        Pattern pattern;
+        pattern = Pattern.compile("12345");
+        Matcher matcher = pattern.matcher(string);
+//        System.out.println(string.substring(matcher.start(), matcher.end())+ "*");
+        while(matcher.find()) {
+            System.out.print(string.substring(matcher.start(), matcher.end()) + "*");
+        }
+        System.out.println("someTEXTEPTA " + matcher);
+    }*/
 /*
         iv_cpu = (ImageView) findViewById(R.id.iv_cpu);
         iv_player = (ImageView) findViewById(R.id.iv_player);
