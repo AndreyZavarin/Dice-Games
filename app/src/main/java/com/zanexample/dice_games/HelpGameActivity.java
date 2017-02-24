c26f577712a7a96f4067e2b2939845877f5aa35d
Zavarin <zawarin.andrey20@yandex.ru>
2017-02-24 14:57:56 +0300

    Create splash screen. Create game menu, help activity.

---
+++ app/src/main/java/com/zanexample/dice_games/HelpGameActivity.java
@@ -0,0 +1,16 @@
+package com.zanexample.dice_games;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Andrey on 23.02.2017.
+ */
+
+public class HelpGameActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle saveInstanceState){
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.activity_help_game);
+    }
+}
